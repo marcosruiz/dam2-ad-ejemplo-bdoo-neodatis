519e56958a17f64f6c5ac4c8d310d70c33c24b34
marcosruiz <marcosruizgarcia@gmail.com>
2021-02-09 17:00:10 +0100

    Cambio nombres

--- src/main/java/tutorial/App04ConsultaConWhere2.java
+++ src/main/java/tutorial/App04ConsultaConWhere2.java
@@ -2,31 +2,32 @@ package tutorial;
 
 import org.neodatis.odb.ODB;
 import org.neodatis.odb.ODBFactory;
-import org.neodatis.odb.OID;
 import org.neodatis.odb.Objects;
 import org.neodatis.odb.core.query.IQuery;
+import org.neodatis.odb.core.query.criteria.ICriterion;
 import org.neodatis.odb.core.query.criteria.Where;
 import org.neodatis.odb.impl.core.query.criteria.CriteriaQuery;
 
 /**
- * Esta clase actualiza un jugador que se llame Maria
+ * Esta clase consulta los jugadores que tienen 14 años de edad
  */
-public class App04UpdateJugador {
+public class App04ConsultaConWhere2 {
     public static void main(String[] args){
         // Abrimos la conexión
         ODB odb = ODBFactory.open("neodatis.test");
 
         // Creamos la consulta
-        IQuery iQuery = new CriteriaQuery(Jugador.class, Where.equal(Jugador.NOMBRE, "Maria"));
+        ICriterion iCriterion = Where.equal(Jugador.EDAD, 14);
+        IQuery iQuery = new CriteriaQuery(Jugador.class, iCriterion);
 
         // Obtenemos el primer resultado
-        Jugador jugador = (Jugador) odb.getObjects(iQuery).getFirst();
+        Objects<Jugador> objects = odb.getObjects(iQuery);
 
-        // Iteramos para obtener todos los jugadores
-        jugador.setDeporte("voley-playa");
-
-        // Actualizamos jugador
-        odb.store(jugador);
+        // Iteramos hasta que no haya siguiente elemento
+        while(objects.hasNext()){
+            Jugador j = objects.next();
+            System.out.println(j);
+        }
 
         // Cerramos la conexión
         odb.close();
--- src/main/java/tutorial/App05UpdateJugador.java
+++ src/main/java/tutorial/App05UpdateJugador.java
@@ -2,32 +2,31 @@ package tutorial;
 
 import org.neodatis.odb.ODB;
 import org.neodatis.odb.ODBFactory;
+import org.neodatis.odb.OID;
 import org.neodatis.odb.Objects;
 import org.neodatis.odb.core.query.IQuery;
-import org.neodatis.odb.core.query.criteria.ICriterion;
 import org.neodatis.odb.core.query.criteria.Where;
 import org.neodatis.odb.impl.core.query.criteria.CriteriaQuery;
 
 /**
- * Esta clase consulta los jugadores que tienen 14 años de edad
+ * Esta clase actualiza un jugador que se llame Maria
  */
-public class App05ConsultaConWhere2 {
+public class App05UpdateJugador {
     public static void main(String[] args){
         // Abrimos la conexión
         ODB odb = ODBFactory.open("neodatis.test");
 
         // Creamos la consulta
-        ICriterion iCriterion = Where.equal(Jugador.EDAD, 14);
-        IQuery iQuery = new CriteriaQuery(Jugador.class, iCriterion);
+        IQuery iQuery = new CriteriaQuery(Jugador.class, Where.equal(Jugador.NOMBRE, "Maria"));
 
         // Obtenemos el primer resultado
-        Objects<Jugador> objects = odb.getObjects(iQuery);
+        Jugador jugador = (Jugador) odb.getObjects(iQuery).getFirst();
 
-        // Iteramos hasta que no haya siguiente elemento
-        while(objects.hasNext()){
-            Jugador j = objects.next();
-            System.out.println(j);
-        }
+        // Iteramos para obtener todos los jugadores
+        jugador.setDeporte("voley-playa");
+
+        // Actualizamos jugador
+        odb.store(jugador);
 
         // Cerramos la conexión
         odb.close();
